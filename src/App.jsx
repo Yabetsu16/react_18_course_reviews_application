89fb4ff728ecd4214753964dbc4c57cc1e8599b0
Yabetsu <it.jabez16@gmail.com>
2024-06-28 16:37:21 +0800

    Add Prev and Next

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import { useState } from "react";
 import people from "./data";
-import {
-	FaChevronCircleLeft,
-	FaChevronCircleRight,
-	FaQuoteRight,
-} from "react-icons/fa";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 const App = () => {
 	const [index, setIndex] = useState(0);
 	const { name, job, image, text } = people[index];
+
+	const nextPerson = () => {
+		setIndex((currentIndex) => {
+			const newIndex = currentIndex + 1;
+			return newIndex;
+		});
+	};
+
+	const prevPerson = () => {
+		setIndex((currentIndex) => {
+			const newIndex = currentIndex - 1;
+			return newIndex;
+		});
+	};
 
 	return (
 		<main>
@@ -22,6 +32,14 @@ const App = () => {
 				<h4 className='author'>{name}</h4>
 				<p className='job'>{job}</p>
 				<p className='info'>{text}</p>
+				<div className='btn-container'>
+					<button className='prev-btn' onClick={prevPerson}>
+						<FaChevronLeft />
+					</button>
+					<button className='next-btn' onClick={nextPerson}>
+						<FaChevronRight />
+					</button>
+				</div>
 			</article>
 		</main>
 	);
